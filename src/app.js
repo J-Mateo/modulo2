56858490ad4a1a3aefbdc997884c5383abec668b
J-Mateo <jessica.mateo.fraile@gmail.com>
2026-05-21 14:16:38 +0200

    Configura app Express y conecta rutas

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import indexRoutes from './routes/index.routes.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(indexRoutes)
+
+export default app
